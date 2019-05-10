7986c5d5719b0bb6ef6c2dc6bc120f9ab107dabc
mariopedregon <mariopedregon93@gmail.com>
2019-05-10 13:07:44 -0600

    Initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import moment from "moment";
 
-export default class App extends Component {
+import  PortfolioContainer from "./portfolio/portfolio-container";
+
+
+export default class app extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Mario Pedregon's Portfolio</h1>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
+        <PortfolioContainer />
       </div>
     );
   }
---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import portfolioItem from "./portfolio-item";
+
+import PortfolioItem from "./portfolio-item";
+export default class PortfolioContainer extends Component {
+    
+    constructor () {
+        super()
+        console.log("portfolio container has rendered")
+    }
+    render(){
+        return(
+            <div>
+                <h2>Portfolio items go here...</h2>
+
+                <PortfolioItem />
+            </div>
+        )
+    }
+}
