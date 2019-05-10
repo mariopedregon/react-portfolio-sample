ca875d3e1cffd129b1b836190a9916d68ca364f6
mariopedregon <mariopedregon93@gmail.com>
2019-05-10 11:21:28 -0600

    first part

---
+++ src/components/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Mario Pedregon's Portfolio</h1>
+      </div>
+    );
+  }
+}
